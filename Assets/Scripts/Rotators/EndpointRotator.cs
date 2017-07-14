24197f8b1df64b942de3ff81775ffc079c338e99
MJPCrepin <manuel@crep.in>
2017-07-15 00:58:08 +0100

    Imported ProBuilder

---
+++ Assets/Scripts/Rotators/EndpointRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndpointRotator : MonoBehaviour {
+
+    public float speed = 1;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.Rotate(new Vector3(0, -30, 0) * speed * Time.deltaTime);
+    }
+}
