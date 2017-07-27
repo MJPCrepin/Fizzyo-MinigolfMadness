bb182b0bbaa6155541086daa206f37fe3f06d04a
MJPCrepin <manuel@crep.in>
2017-07-27 20:26:28 +0100

    Colour shop and preview finished

---
+++ Assets/Scripts/SaveState.cs
@@ -0,0 +1,13 @@
+﻿public class SaveState {
+
+    public int coins = 1000;
+    public int completedLevel = 0;
+
+    public int colourOwned = 0;
+    public int hatOwned = 0;
+    public int trailOwned = 0;
+
+    public int activeColour = 0;
+    public int activeHat = 0;
+    public int activeTrail = 0;
+}
