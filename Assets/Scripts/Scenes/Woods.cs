c7547fcad65c7e1eaf3e7c21ae0090aa646a0a45
MJPCrepin <manuel@crep.in>
2017-08-28 11:52:29 +0100

    Updated Woods level (increased holes)

--- Assets/Scripts/Scenes/Woods.cs
+++ Assets/Scripts/Scenes/Woods.cs
@@ -41,15 +41,15 @@ public class Woods : LevelContent {
                 break;
             case 2:
                 SetNewPosition(-30.7f, 2.4f, -14f);
-                SetPar(2);
+                SetPar(4);
                 break;
             case 3:
                 SetNewPosition(3f, 7.5f, -47f);
-                SetPar(3);
+                SetPar(9);
                 break;
             case 4:
                 SetNewPosition(32.8f, 20f, -45f);
-                SetPar(1);
+                SetPar(4);
                 break;
             case 5:
                 SetNewPosition(-29.8f, 3.75f, -47f);
@@ -63,6 +63,22 @@ public class Woods : LevelContent {
                 SetNewPosition(80.14f, 8.9f, -62.8f);
                 SetPar(3);
                 break;
+            case 8:
+                SetNewPosition(-82.43f, 17.53f, -45.63f);
+                SetPar(10);
+                break;
+            case 9:
+                SetNewPosition(-57f, 0f, -127f);
+                SetPar(12); 
+                break;
+            case 10:
+                SetNewPosition(-69.23f, 9.33f, -46.714f);
+                SetPar(8); 
+                break; 
+            case 11:
+                SetNewPosition(36.5f, 8f, -40.5f);
+                SetPar(8);
+                break; //65 par here
             default: Debug.Log("This shouldn't happen"); StartCoroutine(BackToMainMenu()); break;
         }
     }
